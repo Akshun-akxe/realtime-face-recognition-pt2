9271b3d9b43eb4adfc6b1c16fb2d44238cae7eff
Akshun-akxe <71005076+Akshun-akxe@users.noreply.github.com>
2021-09-10 19:05:03 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup() {
+    canvas = createCanvas(350, 350);
+    canvas.center();
+
+    video = createCapture();
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 350, 350);
+}
